4524160ec0d15845b2ee3333f9d8748b7e2310c2
Dexter <djalquisola@gmail.com>
2023-03-01 01:46:54 +0800

    added setup folders

---
+++ lib/widgets/app_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppButton extends StatefulWidget {
+  const AppButton({super.key});
+
+  @override
+  State<AppButton> createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  const AppText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
