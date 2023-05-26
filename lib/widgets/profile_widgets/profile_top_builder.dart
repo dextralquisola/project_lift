89029d5bc4c35cbbf7c77e8f4eae4ecdc3ba2b4f
Dexter <djalquisola@gmail.com>
2023-05-26 20:30:45 +0800

    further refractor for profile

--- lib/widgets/profile_widgets/profile_top_builder.dart
+++ lib/widgets/profile_widgets/profile_top_builder.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../features/profile/screens/edit_availability_screen.dart';
-import '../../features/profile/screens/profile_edit_screen.dart';
-import '../../features/profile/screens/select_avatar_screen.dart';
-
-import '../../features/profile/widgets/profile_widgets.dart';
+import './profile_top_menu_buttons.dart';
 import '../app_text.dart';
 import '../background_cover.dart';
 
@@ -77,93 +73,15 @@ class ProfileTopBuilder extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Row(
-              children: [
-                IconButton(
-                  constraints: const BoxConstraints(),
-                  onPressed: () async {
-                    await logoutDialog(context);
-                  },
-                  icon: const Icon(Icons.exit_to_app, color: Colors.white),
-                ),
-                PopupMenuButton(
-                  icon: const Icon(Icons.more_vert, color: Colors.white),
-                  itemBuilder: (context) {
-                    return [
-                      PopupMenuItem(
-                        value: 0,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.edit, color: Colors.green),
-                            const SizedBox(width: 5),
-                            AppText(
-                              text: "Edit profile details",
-                            ),
-                          ],
-                        ),
-                      ),
-                      PopupMenuItem(
-                        value: 1,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.edit, color: Colors.green),
-                            const SizedBox(width: 5),
-                            AppText(
-                              text: "Change avatar",
-                            ),
-                          ],
-                        ),
-                      ),
-                      if (isTutor)
-                        PopupMenuItem(
-                          value: 2,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Icon(Icons.edit, color: Colors.green),
-                              const SizedBox(width: 5),
-                              AppText(
-                                text: "Change schedule/availability",
-                                textSize: 12,
-                              ),
-                            ],
-                          ),
-                        ),
-                    ];
-                  },
-                  onSelected: (value) async {
-                    if (value == 0) {
-                      await Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const ProfileEditScreen(),
-                        ),
-                      );
-                      updateState!();
-                    } else if (value == 1) {
-                      await Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const SelectAvatarScreen(),
-                        ),
-                      );
-                      updateState!();
-                    } else if (value == 2) {
-                      await Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => const EditAvailabilityScreen(),
-                        ),
-                      );
-                      updateState!();
-                    }
-                  },
-                ),
-              ],
-            ),
-          )
+          if (!isView)
+            Positioned(
+              top: 0,
+              right: 0,
+              child: ProfileTopMenuButtons(
+                updateState: updateState!,
+                isTutor: isTutor,
+              ),
+            )
         ],
       ),
     );
